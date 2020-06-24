01e23e2788f7d84a2b5f19460469150fa12f3770
kinzlee <chinemeremnwoga@gmail.com>
2020-06-24 13:08:21 +0100

    implementing menu drawer

--- navigation/ShopNavigation.js
+++ navigation/ShopNavigation.js
@@ -3,51 +3,106 @@ import { Platform } from "react-native";
 import { NavigationContainer, DrawerActions } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import {HeaderButtons, Item} from 'react-navigation-header-buttons';
-import HeaderButton from '../components/HeaderButtton';
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButtton";
 import ShopHomeScreen from "../screens/ShopHomeScreen";
 import ProductCartScreen from "../screens/ProductCartScreen";
 import ProductManagerScreen from "../screens/ProductManagerScreen";
 import ProductOrdersScreen from "../screens/ProductOrdersScreen";
 import ProductTitleScreen from "../screens/ProductTitleScreen";
 import EditProductScreen from "../screens/EditProductScreen";
+import { RectButton } from "react-native-gesture-handler";
 
 const Stack = createStackNavigator();
 
 ShopNavigation = () => {
   return (
-      <Stack.Navigator initialRouteName="Shop">
-        <Stack.Screen
-          name="Shop"
-          component={ShopHomeScreen}
-          options={({navigation}) => {
-            return {
-              title="Shop",
-              headerLeft: () => (
-                <HeaderButtons>
-                  <Item 
+    <Stack.Navigator initialRouteName="Shop">
+      <Stack.Screen
+        name="Shop"
+        component={ShopHomeScreen}
+        options={({ navigation }) => {
+          return {
+            title: "Shop",
+            headerLeft: () => (
+              <HeaderButtons>
+                <Item
                   title="menu"
                   iconName="ios-menu"
                   onPress={() => {
                     navigation.dispatch(DrawerActions.openDrawer());
                   }}
-                  />
-                </HeaderButtons>
-              )
-              };
-          }}
-        />
-        <Stack.Screen
-          name="Product Title"
-          component={ProductTitleScreen}
-          options={{ title: "Product Title" }}
-        />
-        <Stack.Screen
-          name="Cart"
-          component={ProductCartScreen}
-          options={{ title: "Cart" }}
-        />
-      </Stack.Navigator>
+                />
+              </HeaderButtons>
+            )
+          };
+        }}
+      />
+      <Stack.Screen
+        name="Product Title"
+        component={ProductTitleScreen}
+        options={{ title: "Product Title" }}
+      />
+      <Stack.Screen
+        name="Cart"
+        component={ProductCartScreen}
+        options={{ title: "Cart" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const OrderNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="ProductOrders"
+        component={ProductOrdersScreen}
+        options={({ navigation }) => {
+          return {
+            title: "Orders",
+            headerLeft: () => (
+              <HeaderButtons>
+                <Item
+                  title="menu"
+                  iconName="ios-menu"
+                  onPress={() => {
+                    navigation.dispatch(DrawerActions.openDrawer());
+                  }}
+                />
+              </HeaderButtons>
+            )
+          };
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ManagerNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="ProductManager"
+        component={ProductManagerScreen}
+        options={({ navigation }) => {
+          return {
+            title: "Manage Product",
+            headerLeft: () => (
+              <HeaderButtons>
+                <Item
+                  title="menu"
+                  iconName="ios-menu"
+                  onPress={() => {
+                    navigation.dispatch(DrawerActions.openDrawer());
+                  }}
+                />
+              </HeaderButtons>
+            )
+          };
+        }}
+      />
+    </Stack.Navigator>
   );
 };
 
@@ -66,14 +121,14 @@ const MyDrawer = () => {
         />
         <Drawer.Screen
           name="ProductOrders"
-          component={ProductOrdersScreen}
+          component={OrderNavigator}
           options={{
             drawerLabel: "Orders"
           }}
         />
         <Drawer.Screen
-          name="Product Manager"
-          component={ProductManagerScreen}
+          name="ProductManager"
+          component={ManagerNavigator}
           options={{
             drawerLabel: "Manage Product"
           }}
