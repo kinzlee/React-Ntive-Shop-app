b7d6146cfd6817be3a52704f92219ffa2bb53ff6
kinzlee <chinemeremnwoga@gmail.com>
2020-06-27 13:32:58 +0100

    functionalities for the order page

--- screens/ProductOrdersScreen.js
+++ screens/ProductOrdersScreen.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import CustomText from "../components/CustomText";
 import PRODUCTS from "../data/dummy-data";
 
 const ProductOrdersScreen = () => {
+  const [toggleView, setToggleView] = useState(false);
+
+  const showHideComponent = () => {
+    if (toggleView == false) {
+      setToggleView(true);
+    } else {
+      setToggleView(false);
+    }
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.itemsContainer}>
@@ -11,20 +21,21 @@ const ProductOrdersScreen = () => {
           <CustomText>Price: {}</CustomText>
           <CustomText>Date: 9999</CustomText>
         </View>
-        <View>
-          <TouchableOpacity>
+        <View style={styles.btn}>
+          <TouchableOpacity onPress={set => showHideComponent(set)}>
             <Text>More</Text>
           </TouchableOpacity>
         </View>
       </View>
-
-      <View style={styles.extendedContainer}>
-        <Text style={styles.textStyle}>Item</Text>
-        <View style={styles.inExtContain}>
-          <CustomText>productName</CustomText>
-          <CustomText>productPrice</CustomText>
+      {toggleView ? (
+        <View style={styles.extendedContainer}>
+          <Text style={styles.textStyle}>Item</Text>
+          <View style={styles.inExtContain}>
+            <CustomText>productName</CustomText>
+            <CustomText>productPrice</CustomText>
+          </View>
         </View>
-      </View>
+      ) : null}
     </View>
   );
 };
@@ -33,19 +44,41 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "flex-start"
+    justifyContent: "flex-start",
+    marginVertical: 20
   },
   itemsContainer: {
-    flexDirection: 'row'
+    flexDirection: "column",
+    marginVertical: 10,
+    paddingVertical: 10,
+    borderColor: "#ccc",
+    borderWidth: 2,
+    marginHorizontal: 10,
+    justifyContent: "space-between",
+    paddingHorizontal: 20
   },
   textItem: {
-    flexDirection: 'column'
+    flexDirection: "row",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1
   },
   extendedContainer: {
-    flexDirection: 
+    flexDirection: "row",
+    borderBottomColor: "#000",
+    borderLeftColor: "#000",
+    borderRightColor: "#000",
+    borderBottomWidth: 2,
+    borderRightWidth: 2,
+    borderLeftWidth: 2,
+    paddingHorizontal: 20,
+    paddingVertical: 15
   },
   inExtContain: {},
-  textStyle: {}
+  textStyle: {},
+  btn: {
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default ProductOrdersScreen;
