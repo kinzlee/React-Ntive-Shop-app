5121191b74024ec956eb8f78fa13adff85ea5394
kinzlee <chinemeremnwoga@gmail.com>
2020-07-22 20:07:33 +0100

    fetching products from the server

--- screens/shopHomeScreen.js
+++ screens/shopHomeScreen.js
@@ -1,13 +1,17 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, {useCallback, useState, useEffect} from "react";
+import { View, Button, Text, StyleSheet, ActivityIndicator } from "react-native";
 import ProductList from "../components/ProductList";
 import PRODUCTS from "../data/dummy-data";
 import HeaderButton from "../components/HeaderButtton";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import actionProduct, { fetchProducts } from '../store/actions/product';
+import colors from '../constants/colors';
 // import * as ActionCart from '../store/actions/cart';
 
 const ShopHomeScreen = ({ navigation, route }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   const availableProduct = useSelector(
     state => state.products.availableProducts
   );
@@ -27,6 +31,41 @@ const ShopHomeScreen = ({ navigation, route }) => {
       )
     });
   }, [navigation]);
+  
+  const dispatch = useDispatch();
+
+  const loadedProducts =useCallback( async () => {
+    setIsLoading(true);
+    try {
+    await dispatch(actionProduct.fetchProducts());
+    } catch (err) {
+      setError(err.message)
+    }
+    setIsLoading(false)
+  }, [dispatch, setIsLoading, setError])
+
+  useEffect(() => {
+    loadedProducts()
+  }, [dispatch, loadedProducts])
+
+  if(error) {
+    return <View style={styles.screen}>
+      <Text> An error occured!</Text>
+      <Button  title="Try Again" onPress={loadedProducts} color={colors.primaryColor} /> 
+    </View>
+  }
+
+  if(isLoading) {
+    <View style={styles.screen}>
+      <ActivityIndicator size="large" color={colors.primaryColor} />
+    </View>
+  }
+
+  if(!isLoading && availableProduct.length === 0) {
+    <View style={styles.screen}>
+      <Text>No Products Found! You shold add some</Text>
+    </View>
+  }
 
   return <ProductList listData={availableProduct} navigation={navigation} />;
 };
--- store/actions/product.js
+++ store/actions/product.js
@@ -1,6 +1,39 @@
+import Product from '../../models/Product';
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const SET_PRODUCTS = "SET_PRODUCTS";
+
+
+export const fetchProducts = () => {
+  return async dispatch => {
+    try {
+    const response = await fetch(
+      "https://shopper-e5714.firebaseio.com/product.json"
+    );
+
+    if(!response.ok) {
+      throw new Error('something went wrong!');
+    }
+
+    const resData = await response.json();
+    const loadedProducts = () => {
+      return Object.keys(resData).map(key => (new Product(
+        key,
+        'u1',
+        resData[key].productName,
+        resData[key].productDescription,
+        resData[key].imageUrl,
+        resData[key].price
+      )))
+    }
+
+    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+  } catch (err) {
+    throw err;
+  }
+  };
+};
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
